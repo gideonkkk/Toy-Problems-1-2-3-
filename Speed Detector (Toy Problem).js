b6d911ed6fb5d647a3e25cb10a69a96d8235560a
Gideon Kariuki <gideon.kariuki@student.moringa.com>
2024-01-09 15:06:58 +0300

    Completed assingnment

---
+++ Speed Detector (Toy Problem).js
@@ -0,0 +1,14 @@
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+    if(speed <= speedLimit){
+        return "OK";   
+    }else {
+        const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+        if  (demeritPoints <= 12) {
+            return ("Points:" + demeritPoints)
+        }else{
+            return ("License suspended");
+        }
+    }
+}
